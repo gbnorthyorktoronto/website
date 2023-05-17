09dfc076c9e855c5d0ac80beb72a7aaab9fd94cc
Debora de Oliveira <deborapoh@gmail.com>
2023-05-16 23:24:58 -0300

    adapt schedule to mobile

--- src/app/(standard)/our-school/schedule/page.tsx
+++ src/app/(standard)/our-school/schedule/page.tsx
@@ -2,6 +2,9 @@
 
 import { Box, styled } from '@mui/material'
 import schedule from '../../../images/weekly-schedule.png'
+import scheduleMobile1 from '../../../images/weekly-schedule-mobile1.png'
+import scheduleMobile2 from '../../../images/weekly-schedule-mobile2.png'
+import scheduleMobile3 from '../../../images/weekly-schedule-mobile3.png'
 import Title from '@/app/components/Title';
 import { backgroundHeight } from '@/app/constants';
 import GGrey from '@/app/components/GGrey';
@@ -21,8 +24,12 @@ export default function Instructors() {
   return (
     <Container>
       <GGrey />
-      <Title mt={16} mb={5}>Our weekly schedule</Title>
+      <Title
+        className="!mt-[25px] sm:!mt-[128px]"
+        mb={5}
+      >Our weekly schedule</Title>
       <Image
+        className="hidden sm:block"
         src={schedule.src}
         alt="Weekly Schedule"
         width={1440}
@@ -30,6 +37,42 @@ export default function Instructors() {
         style={{
           height: 'auto',
           maxWidth: '80%',
+          marginBottom: 80,
+        }}
+      />
+      <Image
+        className="sm:hidden"
+        src={scheduleMobile1.src}
+        alt="Weekly Schedule"
+        width={1000}
+        height={1000}
+        style={{
+          height: 'auto',
+          maxWidth: '90%',
+          marginBottom: 10,
+        }}
+      />
+      <Image
+        className="sm:hidden"
+        src={scheduleMobile2.src}
+        alt="Weekly Schedule"
+        width={1000}
+        height={1000}
+        style={{
+          height: 'auto',
+          maxWidth: '90%',
+          marginBottom: 10,
+        }}
+      />
+      <Image
+        className="sm:hidden"
+        src={scheduleMobile3.src}
+        alt="Weekly Schedule"
+        width={1000}
+        height={1000}
+        style={{
+          height: 'auto',
+          maxWidth: '90%',
           marginBottom: 80,
         }}
       />
